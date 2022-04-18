5ffff5cadc66e51e990010b863b2716d2973542e
sima <sima>
2022-04-18 22:07:03 +0430

    add infinit scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import axios from 'axios';
 import {
   UserCircleIcon,
   HeartIcon,
@@ -8,27 +9,58 @@ import {
   DotsVerticalIcon,
   SearchIcon,
 } from '@heroicons/react/outline';
-import { Popover, Disclosure } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react'
 
-import { getData } from '../lib/photos';
 import Search from '../components/search';
 
-export async function getServerSideProps() {
+/*export async function getServerSideProps() {
   let offset = 1
   const response = await fetch(`http://xoosha.com/ws/1/test.php?offset=${offset}`);
-
+console.log(response)
   return {
     props: {
       data: await response.json()
     },
   };
-}
+}*/
 
-export default function Home({ data }) {
 
+export default function Home() {
+  const [offset, setOffset] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
+  
+  const loadMoreData = async () => {
+    
+    if (search === "") {
+      console.log("search2: ", search)
+      console.log("loading: ", loading)
+      setLoading(true);
+
+      fetch(`http://xoosha.com/ws/1/test.php?offset=${offset}`)
+        .then(response => response.json())
+        .then(json => {
+          setProducts((oldData) => [...oldData, ...json]);
+          setOffset(prev => prev += 1);
+          setLoading(false);
+        })
+    }
+  }
+
+  const handleScroll = (e) => {
+    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
+      loadMoreData();
+    }
+  };
+ 
+  useEffect(() => {
+    loadMoreData();
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
   let regex = new RegExp(search, "g");
-  const searchResult = data.filter(item => regex.test(item.description));
+  const searchResult = products?.filter(item => regex.test(item.description)) || [];
 
   return (
     <div className="">
@@ -71,10 +103,9 @@ export default function Home({ data }) {
 
       <main>
         <div className="pr-6 pl-6 pt-10 pb-10">
-          <div className="lg:columns-5 sm:columns-3 xs:columns-1 gap-4 w-full mx-auto space-y-3 pb-28">
-            {searchResult.map((item) => (
-              <div key={item.page_id} className="group">
-                {console.log(item)}
+          <div className="lg:columns-5 sm:columns-3 xs:columns-1 gap-4 w-full mx-auto space-y-3 pb-6">
+            {(search ? searchResult : products)?.map((item, index) => (
+              <div key={index} className="group">
                 <div className="bg-black border mb-4 relative rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 break-inside-avoid">
                   <img
                     src={item.image_url}
@@ -99,22 +130,13 @@ export default function Home({ data }) {
               </div>
             ))}
           </div>
-          {/*<div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {data.map((item) => (
-              <a key={item.id} href={item.href} className="group">
-                {console.log(item)}
-                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                  <img
-                    src={item.image_url}
-                    alt={item.imageAlt}
-                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{item.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{item.price}</p>
-              </a>
-            ))}
-            </div>*/}
+          {loading &&
+            <div className="w-screen mx-auto flex justify-center">
+              <svg role="status" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+              </svg>
+            </div>}
         </div>
       </main>
 
